0a3b7f51f455365ada49cafdc2c7dddb97df5ce0
Larkin Young <larkin.young@algorand.foundation>
2025-11-16 08:48:38 +0000

    Updated

---
+++ custom-frontmatter.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import { ReflectionKind } from 'typedoc'
+import { MarkdownPageEvent } from 'typedoc-plugin-markdown'
+
+/**
+ * @param {import('typedoc-plugin-markdown').MarkdownApplication} app
+ */
+export function load(app) {
+  app.renderer.on(
+    MarkdownPageEvent.BEGIN,
+    /** @param {import('typedoc-plugin-markdown').MarkdownPageEvent} page */
+    (page) => {
+      if (page.filename === 'README.md') {
+        page.frontmatter = {
+          title: 'Overview',
+          sidebar: { order: 0 },
+          ...page.frontmatter,
+        }
+      }
+      const frontmatterKinds = [
+        ReflectionKind.Class,
+        ReflectionKind.Function,
+        ReflectionKind.Interface,
+        ReflectionKind.Module,
+        ReflectionKind.Enum,
+      ]
+      if (frontmatterKinds.includes(page.model?.kind)) {
+        page.frontmatter = {
+          title: page.model?.name,
+          ...page.frontmatter,
+        }
+      }
+    },
+  )
+}
